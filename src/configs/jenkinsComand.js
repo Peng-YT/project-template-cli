a0032277a53029cae24959ff77cba0b19aea9b3a
it-pengyueteng <pengyueteng@efun.com>
2024-03-29 10:41:27 +0800

    test

--- src/configs/jenkinsComand.js
+++ src/configs/jenkinsComand.js
@@ -2,10 +2,13 @@
  * @Author: 彭越腾
  * @Date: 2024-03-25 16:24:50
  * @LastEditors: 彭越腾
- * @LastEditTime: 2024-03-25 16:25:53
+ * @LastEditTime: 2024-03-29 10:41:23
  * @FilePath: \project-template-cli\src\configs\jenkinsComand.js
  * @Description: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export const command = {
-    "nodeServer": ``
+    "nodeServer": {
+        update: '',
+        rollout: ''
+    }
 }
